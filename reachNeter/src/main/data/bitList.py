bfca7e95dd702b368455fe7cfbaa2c714e9b977c
Levindo Gabriel Taschetto Neto <levindo.neto@inf.ufrgs.br>
2016-08-16 19:06:03 -0300

    Add dstList for list of destinations

--- reachNeter/src/main/data/bitList.py
+++ reachNeter/src/main/data/bitList.py
@@ -5,4 +5,5 @@ import collections
 import pickle
 
 bitList    = []  #__init__ of BitVectorList, the lsb is a action bit # Match
+dstList    = []  # This list contains only information about destination of packages in the network
 actionList = []  # This list contains only information about actions of predicates with the same index of the match informations
