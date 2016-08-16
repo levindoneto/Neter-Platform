4027a63906513cf0518c017e5bfab3f9bee0d418
Levindo Gabriel Taschetto Neto <levindo.neto@inf.ufrgs.br>
2016-08-16 19:05:20 -0300

    Add switch counter

--- reachNeter/src/main/run/main.run.py
+++ reachNeter/src/main/run/main.run.py
@@ -7,8 +7,11 @@ from BitVector import BitVector
 import time
 csvData = "../data/data.csv"
 
-#allow = classBit.makeBitVector(1)
-#deny = classBit.makeBitVector(0)
+
+allow = classBit.makeBitVector(1)
+deny = classBit.makeBitVector(0)
+
+swinc = 0    # Switch counter (for csv file)
 
 # Parsing CSV (Regras)
 auxMatchKey = BitVector(size=0) # __init__ BitVect
@@ -18,20 +21,45 @@ with open(csvData, "rb") as csvfile:
     for row in spamreader:   #Linha a linha do csv
         info_index = 0           # Indice
         classBitList.bitList.append([])
+
+
+
         for ind in range(len(row)):          # Coluna a coluna do csv
+            ''' FAZER NESSE MODELO O ACTION
+            # Catch action informations and put this in a specific list
+            n = 0
+            for rule_id in range(len(classBitList.bitList)):
+                classBitList.actionList.append(classBitList.bitList[n][-1])                           # <<<< NAO VAI TER MAIS ESSE LANCE DE PEGAR O ULTIMO COM -1
+                n += 1
+            '''
+
             row[info_index] = classBit.stringToIntFormated(row[info_index])  #Tudo vira inteiro
             row[info_index] = classBit.makeBitVector(row[info_index])
             classBitList.bitList[rule_index].append(row[info_index])
+
+
+            #adicionar mais uma coluna com o switch, talvez algo antes, que mexa direto no arquivo de maneira separada
+
             info_index += 1
+        if (len(classBitList.bitList[rule_index]) == 0):
+            print "incrementou swinc"
+
+
         rule_index += 1
 
+swinc = 0
+for i in range(len(classBitList.bitList)):
+    if (len(classBitList.bitList[i]) == 0):
+        swinc += 1
+    for j in range(len(classBitList.bitList[0])): # All lists have the same len
 
-# Catch action informations and put this in a specific list
-n=0
-for rule_id in range(len(classBitList.bitList)):
-
-    classBitList.actionList.append(classBitList.bitList[n][-1])
-    n += 1
+        try:
+            print "sw: ", swinc
+            print classBitList.bitList[i][j]
+        except:
+            pass
+#print classBitList.bitList[64][6]
+#print classBitList.bitList[64][9]
 
 new_index = 0
 # Catch match informations and put this in a specific list
@@ -42,11 +70,11 @@ for rule_id in classBitList.bitList:
     classBitList.bitList[new_index] = auxPredicate
     new_index += 1
 
+print ">>>>>", classBitList.actionList ,"<<<<"
 
-
-print classBitList.bitList[0], " -> ", classBitList.actionList[0]
-print classBitList.bitList[2], " -> ", classBitList.actionList[2]
-print type(classBitList.bitList[0]), " -> ", type(classBitList.actionList[0])
+#print classBitList.bitList[0], " -> ", classBitList.actionList[0]
+#print classBitList.bitList[2], " -> ", classBitList.actionList[2]
+#print type(classBitList.bitList[0]), " -> ", type(classBitList.actionList[0])
 #print classBitList.bitList[4][-1]
 
 print (classBitList.bitList[0] in classBitList.bitList)  #So'
@@ -56,6 +84,7 @@ testePack = raw_input("Insert the test package [Bit String]: ")
 testePack = classBit.stringToBit(testePack)
 
 start = time.time()  #__init time
+'''
 if (testePack in classBitList.bitList):
     index_testePack = classBitList.bitList.index(testePack)
     if (classBitList.actionList[index_testePack] == allow):
@@ -65,6 +94,6 @@ if (testePack in classBitList.bitList):
 else:
     print "Package not found"
 print type(testePack)
-
+'''
 end = time.time()
 print (end - start), "seconds"
