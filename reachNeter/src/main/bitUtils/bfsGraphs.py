013f772cea4ebcbb45ed74447be4ba8b3c3277c4
Levindo Gabriel Taschetto Neto <levindo.neto@inf.ufrgs.br>
2016-08-26 18:25:52 -0300

    Add makeGraph Method in bfsGraphs.

--- reachNeter/src/main/bitUtils/bfsGraphs.py
+++ reachNeter/src/main/bitUtils/bfsGraphs.py
@@ -1,71 +1,52 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-from random import randint
-from random import randint
+#!/usr/bin/python
+# coding: UTF-8
+import csv
+from src.main.bitUtils import bitVectorUtils as classBit
+from src.main.data import bitList as classBitList
+from BitVector import BitVector
+import time
+csvData = "../data/data.csv"
 
-def getPrimo():
-	primos = [11 , 13 , 17 , 19 , 23 , 29 , 31 , 37 , 41 , 43 , 47 , 53 , 59 , 61 , 67 , 71 , 73 , 79 , 83 , 89 , 97 , 101 , 103 , 107 , 109 , 113 , 127 , 131 , 137 , 139 , 149 , 151 , 157 , 163 , 167 , 173 , 179 , 181 , 191 , 193 , 197 , 199 , 211 , 223 , 227 , 229 , 233 , 239 , 241 , 251 , 257 , 263 , 269 , 271 , 277 , 281 , 283 , 293 , 307 , 311 , 313 , 317 , 331 , 337 , 347 , 349 , 353 , 359 , 367 , 373 , 379 , 383 , 389 , 397 , 401 , 409 , 419 , 421 , 431 , 433 , 439 , 443 , 449 , 457 , 461 , 463 , 467 , 479 , 487 , 491 , 499 , 503 , 509 , 521 , 523 , 541 , 547 , 557 , 563 , 569 , 571 , 577 , 587 , 593 , 599 , 601 , 607 , 613 , 617 , 619 , 631 , 641 , 643 , 647 , 653 , 659 , 661 , 673 , 677 , 683 , 691 , 701 , 709 , 719 , 727 , 733 , 739 , 743 , 751 , 757 , 761 , 769 , 773 , 787 , 797 , 809 , 811 , 821 , 823 , 827 , 829 , 839 , 853 , 857 , 859 , 863 , 877 , 881 , 883 , 887 , 907 , 911 , 919 , 929 , 937 , 941 , 947 , 953 , 967 , 971 , 977 , 983 , 991 , 997]
-	return primos[randint(0,len(primos)-1)]
+graph = {"a": ["c"],
+         "b": ["c", "e"],
+         "c": ["a", "b", "d", "e"],
+         "d": ["c"],
+         "e": ["c", "b"],
+         "f": []
+         }
 
-def sucessores( node ):
-	if node == 0:
-		return range(2,10,2)
-	elif (randint(0,10) % 2 == 0):
-		filhos = [ node+getPrimo() , node+getPrimo() ]
-		print
-		print "      _%d" % filhos[0]
-		print "%4d {_"    % node
-		print "       %d" % filhos[1]
-		return filhos
-	else:
-		print "       %d]" % node
-		return []
 
-def ehPrimo( num ):
-	cont = 0
-	for i in range(2,num):
-		if num%i == 0: cont+=1
-	return (cont == 0 and num > 2)
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
 
-def funcaoAvaliacao( node ):
-	return ehPrimo( node )
+    return edges
 
-"""
-Profundidade:
-	Lista = []
-	Estado = no_raiz;
-	Repita:
-		Se eh_objetivo( estado )
-			Então retorne SUCESSO
-		Senão inserirNaFrenteDaLista(sucessores(estado))
+def find_isolated_nodes(graph):
+    """ returns a list of isolated nodes. """
+    isolated = []
+    for node in graph:
+        if not graph[node]:
+            isolated += node
+    return isolated
 
-		Se Lista estiverVazia
-			Então retorne FALHA
+''' Convert a integer atomic predicate in a Bitvector atomic predicate
+    @:parameter : BV Lists: switches, match, destination, action
+    @:return    : BV Graph '''
+def make_graph(diffSwitches, Switch_rule, Match, Destination, Action): # Type of all parameters -> List of BitVectors
+    graph = {}
+    for i in range(len(diffSwitches)):
+        ruleList = [] # Format: [[Match0, Destination0, Action0],[Match1, Destionation1, Action1],...] -> BitVector Elements
+        for j in range(len(Switch_rule)):
+            ruleList.append()  #Sublist
+            ruleList[j].append(Match[j])
+            ruleList[j].append(Destination[j])
+            ruleLIst[j].append(Action[j])
+        graph.update(i:Rule_list)
 
-		Estado = Lista[0]
-		RemoverPrimeiroItemDaLista
-"""
-from sys import exit
 
-lista  = []
-estado = 0
-caminho = []
-print 'Caminhando:'
-while True:
-	caminho = caminho + [estado]
-	if funcaoAvaliacao( estado ):
-		print '\nSucesso, objetivo encontrado:', estado
-		print 'Caminho:', caminho
-		exit(0)
-	else:
-		#profundidade: insere no início da lista
-		lista = lista + sucessores( estado )
-		##largura: insere no fim da lista
-		#lista = lista + sucessores( estado )
+    return graph
 
-	if lista == []:
-		print '\nFALHA, não foi possível encontrar objetivo'
-		exit(1)
-
-	estado = lista[0]
-	lista.pop(0)
+#print(find_isolated_nodes(graph))
