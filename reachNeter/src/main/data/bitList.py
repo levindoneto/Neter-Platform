06506f9a438097abbffe429fdd3e372217c2d219
Levindo Gabriel Taschetto Neto <levindo.neto@inf.ufrgs.br>
2016-08-26 16:55:23 -0300

    Add list of different switches in the network.

--- reachNeter/src/main/data/bitList.py
+++ reachNeter/src/main/data/bitList.py
@@ -4,7 +4,8 @@
 import collections
 import pickle
 
-matchList    = []  #__init__ of BitVectorList, the lsb is a action bit # Match
-dstList    = []  # This list contains only information about destination of packages in the network
-actionList = []  # This list contains only information about actions of predicates with the same index of the match informations
-switchList = []  # This list contains only information about switches according the network topology
+matchList     = []  #__init__ of BitVectorList, the lsb is a action bit # Match
+dstList       = []  # This list contains only information about destination of packages in the network
+actionList    = []  # This list contains only information about actions of predicates with the same index of the match informations
+switchList    = []  # This list contains only information about switches according the network topology {switch : rule}
+theSwitchList = [] # List of switches in the network topology
