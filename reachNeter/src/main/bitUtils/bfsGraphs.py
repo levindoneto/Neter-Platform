afa2b84c0545215ce36bd313f9b63dfc1bf962be
L Gabriel T Neto <L Gabriel T Neto>
2016-09-02 01:48:01 -0300

    Add the class of queue

--- reachNeter/src/main/bitUtils/bfsGraphs.py
+++ reachNeter/src/main/bitUtils/bfsGraphs.py
@@ -49,6 +49,34 @@ def make_graph(diffSwitches, Switch_rule, Match, Destination, Action): # Type of
         graph.update({classBit.makeBitVector(i):ruleList})
     return graph
 
+class MyQUEUE: # just an implementation of a queue
+
+	def __init__(self):
+		self.holder = []
+
+	def enqueue(self,val):
+		self.holder.append(val)
+
+	def dequeue(self):
+		val = None
+		try:
+			val = self.holder[0]
+			if len(self.holder) == 1:
+				self.holder = []
+			else:
+				self.holder = self.holder[1:]
+		except:
+			pass
+
+		return val
+
+	def IsEmpty(self):
+		result = False
+		if len(self.holder) == 0:
+			result = True
+		return result
+
+
 #print(find_isolated_nodes(graph))
 
 def BFS(graph,start,end,q):
