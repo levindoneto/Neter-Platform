a489ad6961b82b4797ec7f92db8ca9ddfc39ef11
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2020-05-10 15:47:37 -0300

    Add attacking network, try exceptions, logging mininet service

--- rest/api/services/mininet_service.py
+++ rest/api/services/mininet_service.py
@@ -8,41 +8,67 @@ from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.log import setLogLevel
 from mininet.node import RemoteController
+import logging
 
 DEFAULT_IP = "127.0.0.1"
-
+logger = logging.getLogger("mininet")
 parser = argparse.ArgumentParser(description = "Mininet Script")
 parser.add_argument("--ip", action = "store", dest = "ip", default = DEFAULT_IP, required = False, help = "Controller IP")
 net = None
-setLogLevel('info')
 
 def startNetwork(auto_set_macs, hosts, ip, links, switches):
     net = Mininet(topo = topology.topologyMininet(hosts, switches, links), build = False, autoSetMacs = auto_set_macs, controller = partial(RemoteController, ip = ip))
     #net.addController('c0') # To let the controller in the local mode
-    net.start() # Init the network
+    logger.info("Starting network with the hosts: ", hosts, " within the switches: ", switches)
+    try:
+        net.start() # Init the network
+    except:
+        logger.error("On starting network")
     return True
 
-# Ping between all devices
+# Ping all devices
 def pingAll():
-    net.pingAll()
+    logger.info("Pinging network")
+    try:
+        net.pingAll()
+    except:
+        logger.error("On pinging network")
     return True
 
 def getHosts():
     hosts = dict()
-    for host in net.hosts:
-        hosts.update({str(host):host.IP()})
+    logger.info("Getting hosts")
+    try:
+        for host in net.hosts:
+            hosts.update({str(host):host.IP()})
+        logger.info("Hosts: ", hosts)
+    except:
+        logger.error("On getting hosts")
     return hosts
 
 def stopNetwork():
-    net.stop()
+    logger.info("Stoping network")
+    try:
+        net.stop()
+    except:
+        logger.error("On stoping network")
     return True
 
-def attackNetwork():
-    # for i in range(1, 5000):
-        # print(h3.cmd('ping -c1 %s' % h4.IP()))
-    return "NOT IMPLEMENTED"
+def attackNetwork(level=5000):
+    logger.info("Attacking network with level ", level)
+    try:
+        hosts = net.hosts
+        for i in range(1, level):
+            print(hosts[0].cmd('ping -c1 %s' % hosts[0].IP()))
+    except:
+        logger.error("On attacking network")
+    return True
 
 def closeNetwork():
-    import os
-    os.system("sudo mn -c")
+    logger.info("Closing network")
+    try:
+        import os
+        os.system("sudo mn -c")
+    except:
+        logger.error("On closing network")
     return True
