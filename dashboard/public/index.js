e42f9d0e42662d89ca364671d4ee87559c6b4306
levindoneto <levindogtn@gmail.com>
2020-11-01 23:59:08 +0000

    Add new index server

--- dashboard/public/index.js
+++ dashboard/public/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-app.use(express.static('.'));
-app.get('/', function (req, res, next) {
- res.redirect('/'); 
+app.use(express.static('./'));
+app.get('/', (req, res, next) => {
+ res.redirect('/');
 });
-app.listen(8085, 'localhost');
-console.log('Server is Listening on port 8087');
+app.listen(8087, '0.0.0.0');
+console.log('SERVER UP');
+
--- dashboard/public/server.py
+++ dashboard/public/server.py
@@ -1,7 +1,7 @@
 import http.server
 import socketserver
 
-PORT = 8088
+PORT = 8888
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(('', PORT), Handler)
 print('Server running at the port: ', PORT)
