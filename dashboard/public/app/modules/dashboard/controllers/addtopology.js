afb3c815df5c89a8ed3fe9949f1f3f81d70761c5
levindo neto <levindogtn@gmail.com>
2020-11-11 04:46:42 -0300

    Add support to new links

--- dashboard/public/app/modules/dashboard/controllers/addtopology.js
+++ dashboard/public/app/modules/dashboard/controllers/addtopology.js
@@ -24,16 +24,36 @@ dashboard.controller(
                 var auxOptions = $scope.topology.availableOptions;
                 delete auxOptions['h'.concat(String(hostNumber))]; // to avoid link from hostY to hostY
                 
-                return auxOptions;
+                return 0 !== Object.keys(auxOptions).length?auxOptions:undefined;
             };
 
             $scope.getCurrentOptionsSwitch = function(switchNumber) {
                 var auxOptions = $scope.topology.availableOptions;
                 delete auxOptions['s'.concat(String(switchNumber))]; // to avoid link from switchY to switchY
                 
-                return auxOptions;
+                return 0 !== Object.keys(auxOptions).length?auxOptions:undefined;
             };
             
+            $scope.getNewLinkOptionsHosts = function(host) {
+                var auxOptions = $scope.topology.availableOptions;
+                delete auxOptions[host]; // to avoid link from hostY to hostY
+                for (var i = 0; i < $scope.topology.links[host].length; i++) {
+                    delete auxOptions[$scope.topology.links[host][i]];
+                }
+                
+                return 0 !== Object.keys(auxOptions).length?auxOptions:undefined;
+            };
+            
+            $scope.getNewLinkOptionsSwitches = function(switchDevice) {
+                var auxOptions = $scope.topology.availableOptions;
+                delete auxOptions[switchDevice]; // to avoid link from hostY to hostY
+                for (var i = 0; i < $scope.topology.links[switchDevice].length; i++) {
+                    delete auxOptions[$scope.topology.links[switchDevice][i]];
+                }
+                
+                return 0 !== Object.keys(auxOptions).length?auxOptions:undefined;
+            };
+
             // const topologies = firebase.database().ref(`/users/${userId}/topologies/`);
             // const topologiesList = $firebaseArray(topologies);
             // const topologiesObj = $firebaseObject(topologies);
@@ -61,6 +81,7 @@ dashboard.controller(
                     html: 'Host '.concat($scope.currentHostNumber, ' has been added'),
                   }).then(function() {
                     $scope.currentHostNumber += 1;
+                    $scope.$apply(); // start digestion
                     console.log($scope.topology);
                 })
                 })
@@ -82,7 +103,51 @@ dashboard.controller(
                         html: 'Switch '.concat($scope.currentSwitchNumber, ' has been added'),
                     }).then(function() {
                         $scope.currentSwitchNumber += 1;
+                        $scope.$apply(); // start digestion
+                    })
+                })
+            };
 
+            $scope.addNewLink = function(device) {
+                if ('h' == device.charAt(0).toLowerCase()) {
+                    $scope.addNewLinkHost(device);
+                } else {
+                    $scope.addNewLinkSwitch(device);
+                }
+            };
+
+            $scope.addNewLinkHost = function(host) {
+                swal({
+                  title: 'Add Link for Host ' + host,
+                  input: 'select',
+                  inputOptions: $scope.getNewLinkOptionsHosts(host),
+                  inputPlaceholder: 'Select Device',
+                  showCancelButton: true
+                }).then(function (link) {
+                    $scope.topology.links[host].push(link);
+                    swal({
+                        type: 'success',
+                        html: 'Device '.concat(link, ' has been added as a link to host ', host),
+                    }).then(function() {
+                        $scope.$apply(); // start digestion
+                    })
+                })
+            };
+
+            $scope.addNewLinkSwitch = function(switchDevice) {
+                swal({
+                  title: 'Add Link for Switch ' + switchDevice,
+                  input: 'select',
+                  inputOptions: $scope.getNewLinkOptionsSwitches(switchDevice),
+                  inputPlaceholder: 'Select Device',
+                  showCancelButton: true
+                }).then(function (link) {
+                    $scope.topology.links[switchDevice].push(link);
+                    swal({
+                        type: 'success',
+                        html: 'Device '.concat(link, ' has been added as a link to switch ', switchDevice),
+                    }).then(function() {
+                        $scope.$apply(); // start digestion
                     })
                 })
             };
