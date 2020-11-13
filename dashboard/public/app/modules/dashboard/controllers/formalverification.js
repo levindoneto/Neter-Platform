b7b7fb24005a7096f2cae43498fbfecaedf5fde0
levindo neto <levindogtn@gmail.com>
2020-11-13 12:38:55 -0300

    Add FV files

---
+++ dashboard/public/app/modules/dashboard/controllers/formalverification.js
@@ -0,0 +1,18 @@
+dashboard.controller(
+    'formalverificationController', [
+        '$scope',
+        '$firebaseObject',
+        '$firebaseArray',
+        function (
+            $scope,
+            $firebaseObject,
+            $firebaseArray
+        ) {
+            const vm = this;
+            
+            // const verifications = firebase.database().ref(`/users/${userId}/verifications/`);
+            // const verificationsList = $firebaseArray(verifications);
+            // const verificationsObj = $firebaseObject(verifications);
+        }
+    ]
+);
