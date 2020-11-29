b22507e9687d193c6e7e43b1b3f5d6c4612d9fb7
levindo neto <levindogtn@gmail.com>
2020-11-29 02:58:44 -0300

    Up host

--- dashboard/public/app/modules/dashboard/controllers/conflictsFlowtable.js
+++ dashboard/public/app/modules/dashboard/controllers/conflictsFlowtable.js
@@ -61,7 +61,7 @@ dashboard.controller(
                         preConfirm: function(result) {
                             return new Promise(function(resolve, reject) {
                                 if (result) {
-                                    axios.get('http://localhost:8060/rules/flowtable/conflicts')
+                                    axios.get('http://dawntech.brazilsouth.cloudapp.azure.com:8060/rules/flowtable/conflicts')
                                     .then(function(response){
                                         // save db
                                         response.data.name = inputValue;
