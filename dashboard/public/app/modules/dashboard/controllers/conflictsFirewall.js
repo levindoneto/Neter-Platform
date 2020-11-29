4c012248b8a0e1baa4fe522db93bfefad8a92e25
levindo neto <levindogtn@gmail.com>
2020-11-29 04:18:34 -0300

    Add Rules FW FV to dash

--- dashboard/public/app/modules/dashboard/controllers/conflictsFirewall.js
+++ dashboard/public/app/modules/dashboard/controllers/conflictsFirewall.js
@@ -20,6 +20,7 @@ dashboard.controller(
             const verificationsObj = $firebaseObject(verifications);
             verificationsList.$loaded().then(() => {
                 $scope.verifications = verificationsList;
+                $scope.verificationsObj = verificationsObj;
             });
 
             const topologies = firebase.database().ref(`/users/${$scope.userId}/topologies/`);
@@ -27,11 +28,12 @@ dashboard.controller(
             const topologiesObj = $firebaseObject(topologies);
             topologiesList.$loaded().then(() => {
                 $scope.topologies = topologiesObj;
-                console.log($scope.topologies);
+                $scope.currentTopologyId = topologiesObj.currentTopologyId;
+                $scope.currentTopology = topologiesObj[$scope.currentTopologyId];
             });
-            
-            $scope.verifyRulesFirewall = function() {
-                return true;
+
+            $scope.modal = function(id) {
+                $scope.currentVerification = $scope.verificationsObj[id];
             };
 
             $scope.redirectToFormalVerification = function() {
@@ -40,6 +42,61 @@ dashboard.controller(
 
             $scope.redirectToFormalVerificationFirewall = function() {
                 $state.go('app.formalVerificationFirewall');
+            };
+
+            $scope.verifyRulesFirewall = function() {
+                swal({
+                    title: "Name the Verification",
+                    text: "The name is used as a verification identifier",
+                    input: 'text',
+                    showCancelButton: true,
+                    inputPlaceholder: 'Verification '.concat(verificationsList.length + 1)
+                }).then(function(inputValue) {
+                    inputValue = inputValue? inputValue: 'Verification '.concat(verificationsList.length + 1);
+                    swal({
+                        title: 'Are you sure you want to verify conflicts in the firewalls of the topology '.concat($scope.currentTopology.fullName, '?'),
+                        showCancelButton: true,
+                        confirmButtonText: 'Yes',
+                        cancelButtonText: 'Cancel',
+                        showLoaderOnConfirm: true,
+                        preConfirm: function(result) {
+                            return new Promise(function(resolve, reject) {
+                                if (result) {
+                                    axios.get('http://dawntech.brazilsouth.cloudapp.azure.com:8060/rules/firewall/conflicts')
+                                    .then(function(response){
+                                        // save db
+                                        response.data.name = inputValue;
+                                        verificationsList.$add(response.data).then((verificationId) => {
+                                            auxVid = verificationId.toString().split('/');
+                                            verificationId = auxVid[auxVid[auxVid.length-1]];
+                                            swal({
+                                                title: 'The firewalls from the topology '.concat($scope.currentTopology.fullName, ' have been successfully verified!'),
+                                                text: (response.data.status).concat('\n', 'To check out the details, please check the verification history'),
+                                                icon: 'success',
+                                                showCancelButton: true,
+                                                confirmButtonText: 'Ok',
+                                                cancelButtonText: 'Cancel',
+                                                timer: 10000
+                                            }).then(function(ok) {
+                                                $scope.modal(verificationId);
+                                            });
+                                        });
+                                    })
+                                    .catch(function(error){
+                                        swal({
+                                            title: 'Error on verifying conflicts in the firewalls of the topology '.concat($scope.currentTopology.fullName),
+                                            text: 'Please check its details and try again.',
+                                            icon: 'error',
+                                            button: false,
+                                            timer: 5000
+                                        });
+                                    })
+                                }
+                            });
+                        },
+                        allowOutsideClick: () => !swal.isLoading(),
+                    });
+                });
             };
         }
     ]
